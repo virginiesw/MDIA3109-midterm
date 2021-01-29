752367ec2dc60ec2ec87f97fbb4a865779862851
= <=>
2021-01-28 16:55:52 -0800

    Raquel's comps

---
+++ my-app/src/stories/Indicator.stories.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import Indicator from '../comps/indicator';
+
+
+export default {
+  title: 'Indicator',
+  component: Indicator
+};
+
+
+export const BasicButton = () =>  <Indicator />
+
---
+++ my-app/src/stories/Test.stories.js
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import Indicator from '../comps/indicator';
+
+// export default {
+//     title: 'Example/Indicator',
+//     component: Indicator
+// };
+
+// export const BasicIndicator = () => <Indicator/>
+
+// import React,{useState} from 'react';
+// import Button from '../comps/indicator';
+
+
+// export default {
+//   title: 'Button',
+//   component: Button
+// };
+
+
+// export const BasicButton = () =>  <Button />
+
+
+// import React,{useState} from 'react';
+// import CheckBox from '../comps/indicator';
+
+
+// export default {
+//   title: 'CheckBox',
+//   component: CheckBox
+// };
+
+
+// export const BasicCheckBox = () =>  <CheckBox />
+
+// import React, {useState} from 'react';
+
+// import Menu from '../comps/Test';
+
+// export default {
+//   title:'Example/Menu',
+//   component: Menu
+// };
+// export const DropDownMenu = () => <Menu />
+// export const BasicMenu = () => <Menu />
+
+// export const DefaultExpandedMenu= () => <Menu expand={true} />
+// export const ExpandWithAnotherButton= () => {
+//     const [menu, setMenu] = useState(false);
+//     return <div>
+//     <Menu expand={menu} />
+//     <button onClick={()=>setMenu(!menu)}>Expand Menu</button>
+// </div>;
+// }
+ 
+
